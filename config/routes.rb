ac1a596fce475b15b75ecc69383db1765508c0da
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-04-03 15:46:18 +0100

    message model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "chat_rooms#index"
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
