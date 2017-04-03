60ff31e29ba6b140a466cf100fe30aedc513ee71
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-04-03 16:04:27 +0100

    coffee added to JS channels

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "chat_rooms#index"
   resources :chat_rooms, only: [:new, :create, :show, :index]
+  mount ActionCable.server => '/cable'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
