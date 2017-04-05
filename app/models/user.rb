8b188a1cc890b2ba3178b0507c260990fb7e09d6
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2017-04-05 13:00:00 +0100

    validations for username added ot user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          has_many :chat_rooms, dependent: :destroy
          has_many :messages, dependent: :destroy
+         validates :username, :presence => true, :uniqueness => {
+    :case_sensitive => false
+  }
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 
   def name
     email.split('@')[0]
